8fd866cbae8efb50b5f8e9ab3e5746a2f124c7ef
iisquare <iisquare@163.com>
2025-03-26 20:00:57 +0800

    train

--- src/fs_gpt/train/tuner.py
+++ src/fs_gpt/train/tuner.py
@@ -28,6 +28,8 @@ class Tuner:
         training_args = TrainingArguments(
             output_dir=self.output_dir,
             logging_dir=self.output_dir,
+            do_train=self.args.get("do_train", True),
+            do_eval=self.args.get("do_eval", True),
             overwrite_output_dir=self.args.get("overwrite_output_dir", False),
             num_train_epochs=self.args.get("num_train_epochs", 3.0),
             max_steps=self.max_steps,
@@ -36,11 +38,11 @@ class Tuner:
             lr_scheduler_type=self.args.get("lr_scheduler_type", "cosine"),
             warmup_ratio=self.args.get("warmup_ratio", 0.1),
             bf16=self.args.get("bf16", True),
-            ddp_timeout=self.args.get("ddp_timeout", 180000000),
+            ddp_timeout=self.args.get("ddp_timeout", 1800),
             save_steps=self.args.get("save_steps", 500),
             logging_steps=self.args.get("logging_steps", 10),
             per_device_eval_batch_size=self.args.get("per_device_eval_batch_size", 1),
-            evaluation_strategy=self.args.get("evaluation_strategy", "steps"),
+            eval_strategy=self.args.get("eval_strategy", "steps"),
             eval_steps=self.args.get("eval_steps", 500),
             deepspeed=self.args.get("deepspeed"),
         )
