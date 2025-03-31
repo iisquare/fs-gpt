6c1b8ec53bb92cb7439c5dcfe0897e404eef5937
iisquare <iisquare@163.com>
2025-03-31 19:48:44 +0800

    lora config

--- sbin/vllm.sh
+++ sbin/vllm.sh
@@ -3,15 +3,18 @@
 # https://docs.vllm.ai/en/latest/serving/engine_args.html
 
 PROJECT_ROOT=$(dirname $(dirname $(readlink -f $0)))
-MODEL_PATH=${PROJECT_ROOT}/models/Qwen2.5-0.5B
+MODEL_PATH=${PROJECT_ROOT}/models/Qwen2.5-0.5B-Instruct
+LORA_PATH=${PROJECT_ROOT}/models/qwen-sft-lora
 
 export CUDA_VISIBLE_DEVICES=0
 
 cmd=$(cat <<- EOF
 vllm serve ${MODEL_PATH} \
 --served-model-name $(basename ${MODEL_PATH}) \
+--enable-lora \
+--lora-modules qwen-sft-lora=${LORA_PATH} \
 --tensor-parallel-size 1 \
---max-model-len 32768 \
+--max-model-len 128 \
 --gpu-memory-utilization 0.9 \
 --max-num-seqs 8 \
 --enforce-eager \
