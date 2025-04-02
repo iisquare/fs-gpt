7629d991a140dccd90bef202cc84641967c1d7d7
iisquare <iisquare@163.com>
2025-04-02 18:06:20 +0800

    sglang

--- sbin/sglang-lora.sh
+++ sbin/sglang-lora.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# https://docs.sglang.ai/backend/server_arguments.html
+# https://docs.vllm.ai/en/latest/serving/engine_args.html
 
 PROJECT_ROOT=$(dirname $(dirname $(readlink -f $0)))
-MODEL_PATH=${PROJECT_ROOT}/models/Qwen2.5-0.5B
+MODEL_PATH=${PROJECT_ROOT}/models/Qwen2.5-0.5B-Instruct
+LORA_PATH=${PROJECT_ROOT}/models/qwen-sft-lora
 
 export CUDA_VISIBLE_DEVICES=0
 
 cmd=$(cat <<- EOF
 python -m sglang.launch_server --model-path ${MODEL_PATH} \
+--lora-paths $(basename ${LORA_PATH})=${LORA_PATH} \
+--disable-radix-cache \
+--disable-cuda-graph \
 --trust-remote-code \
 --tp 1 \
+--dp 1 \
 --host 0.0.0.0 \
 --port 6011
 EOF
